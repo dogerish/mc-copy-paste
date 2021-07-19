50040a08dad40bda6651be30506665e39a2e2ea7
dogerish <luca.filmstrip@gmail.com>
2021-07-18 22:03:27 -0400

    Make improvements to index.py
    Copy pasting continues endlessly instead of once per run, and messages
    are logged to both the console and the chat. More, better verbose and
    fix constant copying while waiting for paste.

--- index.py
+++ index.py
@@ -1,20 +1,25 @@
 from selection      import Selection, readsel
 from mcpi.minecraft import Minecraft as MC
 from time import sleep
+def log(message: str):
+    print(message)
+    mc.postToChat(message)
 
-mc = MC.create()
-mc.postToChat("Connected.")
-
+mc   = MC.create()
 poss = []
+sel  = Selection([[[]]], mc)
+log("Ready.")
 
 while True:
     for e in mc.events.pollBlockHits():
         poss.append(e.pos)
-        mc.postToChat(f"{e.pos.x}, {e.pos.y}, {e.pos.z}")
-    if len(poss) >= 2:
-        sel = readsel(poss[0], poss[1], mc)
-        with open("sel.txt", "w+") as f:
-            f.write(str(sel.blocks))
-    if len(poss) >= 3:
-        sel.paste(poss[2])
+        info = f"Endpoint {len(poss)}" if len(poss) < 3 else "Pasting to"
+        log(f"{info}: {e.pos.x}, {e.pos.y}, {e.pos.z}")
+        if len(poss) == 2:
+            log("Copying selection.")
+            sel = readsel(poss[0], poss[1], mc)
+        elif len(poss) > 2:
+            log("Pasting selection.")
+            sel.paste(poss[2])
+            poss.clear()
     sleep(0.1)
